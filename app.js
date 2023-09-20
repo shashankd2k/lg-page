bdbce04003af7f09f3b4fbd1fa7eeec8352f7648
shashankd2k <shashankd859@gmail.com>
2023-09-20 12:32:18 +0530

    My fist GPT welcome file added

---
+++ app.js
@@ -0,0 +1,22 @@
+function App() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+          </p>
+          <a
+            className="App-link"
+            href="login.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Login page
+          </a>
+        </header>
+      </div>
+    );
+  }
+  
+  export default App;
+  
